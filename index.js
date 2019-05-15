57b8d2b1740fd8fbbfc77304bf3301060cfafd51
whapplez <whapplez@gmail.com>
2019-05-15 12:38:29 -0400

    draggable

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 
 var express = require('express')
 var app = express();
+app.use(express.static(__dirname ));
 
 // -------------- express initialization -------------- //
 // PORT SETUP - NUMBER SPECIFIC TO THIS SYSTEM
