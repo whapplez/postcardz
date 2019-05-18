3b163f1b4e13f4e757f51d3a838a4ba0ddeb5094
whapplez <whapplez@gmail.com>
2019-05-18 01:25:22 -0400

    lotsofpolaroids

--- js/drag.js
+++ js/drag.js
@@ -10,3 +10,21 @@ $(function() {
    $(function() {
        $( "#Draggable4" ).draggable();
     });
+    $(function() {
+        $( "#Draggable5" ).draggable();
+     });
+     $(function() {
+         $( "#Draggable6" ).draggable();
+      });
+      $(function() {
+          $( "#Draggable7" ).draggable();
+       });
+       $(function() {
+           $( "#Draggable8" ).draggable();
+        });
+        $(function() {
+            $( "#Draggable9" ).draggable();
+         });
+         $(function() {
+             $( "#Draggable10" ).draggable();
+          });
